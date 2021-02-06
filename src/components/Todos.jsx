3999915595589d4cc844cc91da33aa39158f27ff
devcopas <73013599+devcopas@users.noreply.github.com>
2021-02-06 17:43:15 +0700

    menambahkan fungsi completeTodo

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -2,11 +2,19 @@ import React from "react";
 import PropTypes from "prop-types";
 import Todo from "./Todo";
 
-const Todos = ({ todos }) => {
+const Todos = ({ todos, completeTodo }) => {
   return (
     <section className="todos">
       {todos.map((t, i) => {
-        return <Todo key={i} text={t.text} />;
+        return (
+          <Todo
+          key={i}
+          text={t.text}
+          completeTodo={completeTodo}
+          index={i}
+          isCompleted={t.isCompleted}
+          />
+        );
       })}
     </section>
   );
@@ -17,7 +25,8 @@ Todos.propTypes = {
     PropTypes.shape({
       text: PropTypes.string
     })
-  )
+  ),
+  completeTodo: PropTypes.func.isRequired
 };
 
 export default Todos;
--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -6,26 +6,31 @@ import Todos from "../components/Todos";
 
 const TodoList = () => {
   const [todos, setTodos] = useState([
-    { text: "Belajar react!" },
-    { text: "Belajar hooks" },
-    { text: "Belajar component" }
+    { text: "Belajar react!", isCompleted: false },
+    { text: "Belajar hooks", isCompleted: false },
+    { text: "Belajar component", isCompleted: false }
   ]);
 
   const [showAdd, setShowAdd] = useState(false);
 
   const addTodo = (value) => {
-    const addedTodo = [...todos, { text: value }];
+    const addedTodo = [...todos, { text: value, isCompleted: false }];
+    setTodos(addedTodo);
+  };
+
+  const completeTodo = (index) => {
+    const addedTodo = [...todos];
+    addedTodo[index].isCompleted = !addedTodo[index].isCompleted;
     setTodos(addedTodo);
   };
 
   const showAddToggle = () => setShowAdd(!showAdd);
-  console.log(showAdd);
 
   return (
     <Paper>
       <Header showAddToggle={showAddToggle} showAdd={showAdd} />
       <TodoForm addTodo={addTodo} showAdd={showAdd} />
-      <Todos todos={todos} />
+      <Todos todos={todos} completeTodo={completeTodo} />
     </Paper>
   );
 };
