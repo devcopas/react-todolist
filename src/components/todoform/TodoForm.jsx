106953d261d5d47704d0a33496d67f9c9cbe7266
devcopas <73013599+devcopas@users.noreply.github.com>
2021-02-08 19:39:17 +0700

    membuat css module untuk todoform

--- src/components/todoform/TodoForm.jsx
+++ src/components/todoform/TodoForm.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
+import styles from "./todoform.module.css";
 
-const TodoForm = ({ addTodo, showAdd}) => {
+const TodoForm = ({ addTodo, showAdd }) => {
   const [value, setValue] = useState("");
 
   const handleFormSubmit = (e) => {
@@ -23,16 +24,16 @@ const TodoForm = ({ addTodo, showAdd}) => {
 
   if (showAdd) {
     return (
-      <section className="add">
-        <form className="add-form" onSubmit={handleFormSubmit}>
+      <section className={styles.add}>
+        <form className={styles.addForm} onSubmit={handleFormSubmit}>
           <input
             type="text"
-            className="add-input"
+            className={styles.addInput}
             value={value}
             autoFocus={true}
             onChange={(e) => setValue(e.target.value)}
           />
-          <button className="add-btn">Save</button>
+          <button className={styles.addBtn}>Save</button>
         </form>
       </section>
     );
