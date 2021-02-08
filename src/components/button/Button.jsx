5d3e9e849119d4a85d554743bd3c40f4ac2043f8
devcopas <73013599+devcopas@users.noreply.github.com>
2021-02-08 18:56:29 +0700

    membuat button dengan css module

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
+import cx from "classnames";
+import styles from "./button.module.css";
 
 const Button = ({ text, onClick, color }) => {
-  const classNames = [
-    "header-btn",
-    color === "blue" && "main-blue-color",
-    color === "red" && "main-red-color"
-  ].join(" ");
+  // const classNames = [
+  //   "header-btn",
+  //   color === "blue" && "main-blue-color",
+  //   color === "red" && "main-red-color"
+  // ].join(" ");
+
+  const classNames = cx(styles.headerBtn, {
+    [styles.mainBlueColor]: color === "blue",
+    [styles.mainRedColor]: color === "red"
+  });
 
   return (
     <button className={classNames} onClick={onClick}>
