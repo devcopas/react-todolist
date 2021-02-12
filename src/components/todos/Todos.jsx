ce10a70427069f767f962a51fe8466b68cb9d43f
devcopas <73013599+devcopas@users.noreply.github.com>
2021-02-13 00:00:57 +0700

    membuat emotion style pada Todos component

--- src/components/todos/Todos.jsx
+++ src/components/todos/Todos.jsx
@@ -1,11 +1,42 @@
-import React from "react";
+/** @jsxImportSource @emotion/react */
+import { useTheme } from "@emotion/react";
+import * as styles from "./todos.styles";
+
 import PropTypes from "prop-types";
 import Todo from "../todo/Todo";
-import styles from "./todos.module.css";
+// import React from "react";
+// import styles from "./todos.module.css";
 
 const Todos = ({ todos, completeTodo }) => {
+  const theme = useTheme();
+
   return (
-    <section className={styles.todos}>
+    // <section className={styles.todos}>
+    //   {todos.length > 0 &&
+    //     todos.map((t, i) => {
+    //       return (
+    //         <Todo
+    //           key={i}
+    //           text={t.text}
+    //           completeTodo={completeTodo}
+    //           index={i}
+    //           isCompleted={t.isCompleted}
+    //         />
+    //       );
+    //     })}
+    //   {todos.length === 0 && (
+    //     <div className={styles.todosPlaceholder}>
+    //       <p>"No todos available"</p>
+    //       <p>
+    //         Add todo by clicking{" "}
+    //         <span className={styles.todosPlaceholderHighlightText}>Add</span>{" "}
+    //         button on the top left corner.
+    //       </p>
+    //     </div>
+    //   )}
+    // </section>
+
+    <section css={styles.todos}>
       {todos.length > 0 &&
         todos.map((t, i) => {
           return (
@@ -19,12 +50,12 @@ const Todos = ({ todos, completeTodo }) => {
           );
         })}
       {todos.length === 0 && (
-        <div className={styles.todosPlaceholder}>
+        <div css={styles.todosPlaceholder(theme)}>
           <p>"No todos available"</p>
           <p>
             Add todo by clicking{" "}
-            <span className={styles.todosPlaceholderHighlightText}>Add</span>{" "}
-            button on the top left corner.
+            <span css={styles.highlightText(theme)}>Add</span> button on the top
+            left corner.
           </p>
         </div>
       )}
---
+++ src/components/todos/todos.styles.js
@@ -0,0 +1,23 @@
+import { css } from "@emotion/react";
+
+export const todos = () =>
+  css`
+    display: flex;
+    flex-direction: column;
+    min-height: 500px;
+  `;
+
+export const todosPlaceholder = (theme) =>
+  css`
+    color: ${theme.color.light.base};
+    text-align: center;
+    font-size: 1.8rem;
+    font-weight: 500;
+    padding: 1rem 10rem;
+  `;
+
+export const highlightText = (theme) =>
+  css`
+    font-family: "Bungee", sans-serif;
+    color: ${theme.color.primary.base};
+  `;
