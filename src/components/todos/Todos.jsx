3076f9ecb9b2dbf8b7c312f4eccce904ecd39934
devcopas <73013599+devcopas@users.noreply.github.com>
2021-02-15 20:58:55 +0700

    implementasi layout container dan item pada Todos component

--- src/components/todos/Todos.jsx
+++ src/components/todos/Todos.jsx
@@ -4,6 +4,8 @@ import * as styles from "./todos.styles";
 
 import PropTypes from "prop-types";
 import Todo from "../todo/Todo";
+import Container from "../../layout/Container";
+
 // import React from "react";
 // import styles from "./todos.module.css";
 
@@ -36,29 +38,56 @@ const Todos = ({ todos, completeTodo }) => {
     //   )}
     // </section>
 
-    <section css={styles.todos}>
-      {todos.length > 0 &&
-        todos.map((t, i) => {
-          return (
-            <Todo
-              key={i}
-              text={t.text}
-              completeTodo={completeTodo}
-              index={i}
-              isCompleted={t.isCompleted}
-            />
-          );
-        })}
-      {todos.length === 0 && (
-        <div css={styles.todosPlaceholder(theme)}>
-          <p>"No todos available"</p>
-          <p>
-            Add todo by clicking{" "}
-            <span css={styles.highlightText(theme)}>Add</span> button on the top
-            left corner.
-          </p>
-        </div>
-      )}
+    // <section css={styles.todos}>
+    //   {todos.length > 0 &&
+    //     todos.map((t, i) => {
+    //       return (
+    //         <Todo
+    //           key={i}
+    //           text={t.text}
+    //           completeTodo={completeTodo}
+    //           index={i}
+    //           isCompleted={t.isCompleted}
+    //         />
+    //       );
+    //     })}
+    //   {todos.length === 0 && (
+    //     <div css={styles.todosPlaceholder(theme)}>
+    //       <p>"No todos available"</p>
+    //       <p>
+    //         Add todo by clicking{" "}
+    //         <span css={styles.highlightText(theme)}>Add</span> button on the top
+    //         left corner.
+    //       </p>
+    //     </div>
+    //   )}
+    // </section>
+
+    <section className="todos-component">
+      <Container flexDirection="column" minHeight="500px">
+        {todos.length > 0 &&
+          todos.map((t, i) => {
+            return (
+              <Todo
+                key={i}
+                text={t.text}
+                completeTodo={completeTodo}
+                index={i}
+                isCompleted={t.isCompleted}
+              />
+            );
+          })}
+        {todos.length === 0 && (
+          <div css={styles.todosPlaceholder(theme)}>
+            <p>"No todos available"</p>
+            <p>
+              Add todo by clicking{" "}
+              <span css={styles.highlightText(theme)}>Add</span> button on the
+              top left corner.
+            </p>
+          </div>
+        )}
+      </Container>
     </section>
   );
 };
